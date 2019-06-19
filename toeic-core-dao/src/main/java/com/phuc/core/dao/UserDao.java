60f2b658f9ea6777ef43d7d6d6e555a5e195b72a
NgoQuangPhuc <ngoquangphucx5ql@gmail.com>
2019-06-19 10:37:54 +0700

    create function login v3

--- toeic-core-dao/src/main/java/com/phuc/core/dao/UserDao.java
+++ toeic-core-dao/src/main/java/com/phuc/core/dao/UserDao.java
@@ -4,4 +4,6 @@ import com.phuc.core.data.dao.GenericDao;
 import com.phuc.core.persistence.entity.UserEntity;
 
 public interface UserDao extends GenericDao<Integer, UserEntity> {
+    UserEntity isUserExist(String name, String password);
+    UserEntity findRoleByUser(String name, String password);
 }
--- toeic-core-test/src/test/java/com/phuc/api/test/LoginTest.java
+++ toeic-core-test/src/test/java/com/phuc/api/test/LoginTest.java
@@ -1,4 +1,34 @@
 package com.phuc.api.test;
 
+
+import com.phuc.core.dao.UserDao;
+import com.phuc.core.daoimpl.UserDaoImpl;
+import com.phuc.core.persistence.entity.UserEntity;
+import org.apache.log4j.Logger;
+import org.testng.annotations.Test;
+
 public class LoginTest {
+    private final Logger log = Logger.getLogger(this.getClass());
+
+    @Test
+    public void checkIsUserExist() {
+        UserDao userDao = new UserDaoImpl();
+        String name = "ngoquangphuc";
+        String password = "123456";
+        UserEntity entity = userDao.isUserExist(name, password);
+        if (entity != null) {
+            log.error("Login success");
+        } else {
+            log.error("Login fail");
+        }
+    }
+
+    @Test
+    public void checkFindRoleByUser() {
+        UserDao userDao = new UserDaoImpl();
+        String name = "ngoquangphuc";
+        String password = "123456";
+        UserEntity entity = userDao.findRoleByUser(name, password);
+        log.error(entity.getRoleEntity().getRoleId() + "-" + entity.getRoleEntity().getName());
+    }
 }
