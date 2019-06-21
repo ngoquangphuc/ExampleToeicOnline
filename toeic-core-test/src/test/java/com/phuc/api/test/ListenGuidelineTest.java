f9f4859a247ada587c12b9b40bb15ddd805a2b7b
NgoQuangPhuc <ngoquangphucx5ql@gmail.com>
2019-06-21 19:00:27 +0700

    test api findByProperty

--- toeic-core-test/src/test/java/com/phuc/api/test/ListenGuidelineTest.java
+++ toeic-core-test/src/test/java/com/phuc/api/test/ListenGuidelineTest.java
@@ -2,12 +2,31 @@ package com.phuc.api.test;
 
 import com.phuc.core.dao.ListenGuidelineDao;
 import com.phuc.core.daoimpl.ListenGuidelineDaoImpl;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ListenGuidelineTest {
+    ListenGuidelineDao listenGuidelineDao;
+
+    @BeforeTest
+    public void initData() {
+        listenGuidelineDao = new ListenGuidelineDaoImpl();
+    }
+
     @Test
     public void testFindByProperties() {
-        ListenGuidelineDao listenGuidelineDao =  new ListenGuidelineDaoImpl();
-        Object result = listenGuidelineDao.findByProperty(null, null, null, null, 2, 2);
+//        ListenGuidelineDao listenGuidelineDao = new ListenGuidelineDaoImpl();
+//        Object result = listenGuidelineDao.findByProperty(null, null, null, null, 2, 2);
+    }
+
+    @Test
+    public void checkApiFindByProperty() {
+        Map<String, Object> property = new HashMap<String, Object>();
+        property.put("title", "Bai hd 8");
+        property.put("content", "Noi dung bai hd 8");
+        Object[] objects = listenGuidelineDao.findByProperty(property, null, null, null, null);
     }
 }
